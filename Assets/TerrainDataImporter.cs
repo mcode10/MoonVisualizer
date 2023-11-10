d574038bbea7c90edba62a4122df0207990926b4
Manas Paranjape <manas@designX6.com>
2023-11-10 13:22:44 -0500

    Fixes several bugs in TerrainDataImporter.

--- Assets/TerrainDataImporter.cs
+++ Assets/TerrainDataImporter.cs
@@ -8,7 +8,7 @@ public class TerrainDataImporter : MonoBehaviour
 {
     // Constants for normalizing the height.
     const float minHeight = -418.9165344238281f;
-    const float maxHeight = -1648.679809570313f;
+    const float maxHeight = 1648.679809570313f;
 
     // Terrain size constant.
     const int terrainSize = 3200;
@@ -56,12 +56,7 @@ public class TerrainDataImporter : MonoBehaviour
                 point.Insert(2, normalizedHeight);
 
                 points.Insert(count, point);
-                count += count;
-
-                if (count == 0)
-                {
-                    Console.WriteLine(point);
-                }
+                count++;
             }
         }
 
@@ -71,22 +66,23 @@ public class TerrainDataImporter : MonoBehaviour
     List<List<float>> ScaleData(List<List<float>> points)
     {
         int index = 0;
+        List<List<float>> scaledPoints = new List<List<float>>();
         foreach (List<float> point in points)
         {
-            float scalingConstant = 5f;
+            float scalingConstant = 2f;
 
             float latitude = point[0] * scalingConstant;
             float longitude = point[1] * scalingConstant;
-            float height = point[2] * scalingConstant;
+            float height = point[2];
 
-            points.Remove(point);
+            // points.Remove(point);
             List<float> scaledPoint = new List<float>();
 
             scaledPoint.Insert(0, latitude);
             scaledPoint.Insert(1, longitude);
             scaledPoint.Insert(2, height);
 
-            points.Insert(index, scaledPoint);
+            scaledPoints.Insert(index, scaledPoint);
             index++;
         }
 
@@ -96,10 +92,10 @@ public class TerrainDataImporter : MonoBehaviour
 
     float[,] ConvertScaledDataTo2DArray(List<List<float>> points)
     {
-        float[,] heights = new float[points.Count, points.Count];
+        float[,] heights = new float[3200, 3200];
         foreach (List<float> point in points)
         {
-            heights[(int)Math.Round(point[0]), (int)Math.Round(point[1])] = point[0];
+            heights[(int)Math.Round(point[0]), (int)Math.Round(point[1])] = point[2];
         }
 
         return heights;
